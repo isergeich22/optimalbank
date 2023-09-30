02bcf8791a0b5cb27d08c04df5b271ca1db5d847
ilya <skiing1r@gmail.com>
2023-09-30 18:57:44 +0300

    add markers on map

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,5 +1,8 @@
 import React from "react";
-import { YMaps, Map } from "@pbe/react-yandex-maps"
+import { YMaps, Map, SearchControl, Placemark } from "@pbe/react-yandex-maps"
+import Departments from "../Data/departments.json"
+
+const departments = Departments
 
 export default function MapBlock() {
 
@@ -8,7 +11,18 @@ export default function MapBlock() {
         <>
             <YMaps>
                 <div className="map-block">
-                    <Map defaultState={{ center: [55.75, 37.57], zoom: 9}} width={"100vw"} height={"100vh"}/>
+                    <Map defaultState={{ center: [55.75, 37.57], zoom: 9}} width={"100vw"} height={"94vh"}>
+                        <SearchControl options={{float: "right"}}/>
+                        
+                        
+                        
+                            <Placemark geometry={departments[0].depCoords}/>
+                            <Placemark geometry={departments[1].depCoords}/>
+                            <Placemark geometry={departments[2].depCoords}/>
+                        
+                        
+
+                    </Map>
                 </div>
             </YMaps>
         </>
