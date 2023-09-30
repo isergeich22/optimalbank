a5a102f7f6cc09ee89b1ab1638d50988b186c67a
ilya <skiing1r@gmail.com>
2023-10-01 00:51:18 +0300

    add geolocation check

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,17 +1,18 @@
 import React from "react";
-import { YMaps, Map, SearchControl } from "@pbe/react-yandex-maps"
+import { YMaps, Map, SearchControl, GeolocationControl } from "@pbe/react-yandex-maps"
 import Marks from "./Subcomponents/Marks";
-
 export default function MapBlock() {
 
     return(
 
         <>
-            <YMaps>
+            <YMaps query={{apikey: '2d3deabb-4236-45c3-a61b-b92127f41286', lang: 'en_RU'}}>
                 <div className="map-block">
                     <Map defaultState={{ center: [55.75, 37.57], zoom: 9}} width={"100vw"} height={"94vh"}>
+                        <GeolocationControl />
                         <SearchControl options={{float: "right"}}/>
 
+                                
                                 <Marks/>
 
                     </Map>
