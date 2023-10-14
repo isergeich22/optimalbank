68d96d558b76fa57731b7b7756f1ea7508ec2bc9
Wokkta <artem_klesov@mail.ru>
2023-10-14 16:28:03 +0300

    cors

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ import EnvironmentPlugin from 'vite-plugin-environment';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), EnvironmentPlugin('all', { prefix: 'REACT_APP_' })],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://195.93.252.223:8081/api/v1',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 });
